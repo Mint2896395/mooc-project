92ae61a866d50d30d4080751c0ed34568134993a
mint289365 <mint289365@gmail.com>
2023-07-01 04:51:57 +0700

    CardCourse

---
+++ components/Card.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import Image from "next/image";
+
+interface Course {
+  id: any;
+  name: string;
+  src: string;
+  code: string;
+  url: string;
+  category: string;
+  seat: string;
+  status: string;
+}
+
+interface CourseProps {
+  course: Course;
+}
+
+const CardCourse: React.FC<CourseProps> = ({ course }) => {
+  return (
+    <Link href={course.url} key={course.id}>
+      <div className="group w-full scale-95 transform cursor-pointer overflow-hidden border-0 border-b-2 border-solid border-[var(--global-color-primary)] transition duration-500 hover:scale-100 md:h-full md:max-w-lg ">
+          <Image
+          className="w-full rounded-xl"
+          src={course.src}
+          width={640}
+          height={360}
+          alt={course.name}
+          />
+          <div className="md:py-4">
+          <div className="mb-5 h-10 min-h-0 md:text-2xl text-base font-semibold md:font-bold md:mb-0 md:min-h-full group-hover:text-gray-500">
+              {course.name}
+          </div>
+          <p className="md:mb-5 mb-2 md:text-lg text-sm text-gray-700">
+              {course.code}
+          </p>
+          <div className="mb-2 box-border grid grid-cols-1 md:grid-cols-2">
+              <div className="pb-2 text-center text-xs md:text-left">
+              <span className="mr-2 inline-block rounded-full border border-orange-400 px-2 py-0.5 text-orange-400 ">
+                  {course.category}
+              </span>
+              </div>
+              <div className="box-border text-center md:text-right ">
+              <span className="rounded-sm bg-[#4d40f7] px-2 py-0.5 text-xs font-medium text-[#ffee00]">
+                  {course.seat}
+              </span>
+              </div>
+          </div>
+          </div>
+      </div>
+    </Link>
+  );
+};
+
+export default CardCourse;
--- components/CardSlider.tsx
+++ components/CardSlider.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import * as $ from "jquery";
 
 declare global {
@@ -13,12 +12,12 @@ if (typeof window !== "undefined") {
   window.$ = window.jQuery = $;
 }
 
-import Link from "next/link";
 import dynamic from "next/dynamic";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
 import { coursesp, nextIcon, prevIcon } from "../constant/course"
+import CardCourse from "./Card";
 
 // This is for Next.js. On Rect JS remove this line
 const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
@@ -70,37 +69,7 @@ const CardSlider = () => {
               ? coursesp.filter((course) => course.status == 'feature').map((course) => {
                   return (
                     <>
-                      <Link href={course.url} key={course.id}>
-                        <div className="group w-full scale-95 transform cursor-pointer overflow-hidden border-0 border-b-2 border-solid border-[var(--global-color-primary)] transition duration-500 hover:scale-100 md:h-full md:max-w-lg ">
-                          <Image
-                            className="w-full rounded-xl"
-                            src={course.src}
-                            width={640}
-                            height={360}
-                            alt={course.name}
-                          />
-                          <div className="md:py-4">
-                            <div className="mb-5 h-10 min-h-0 md:text-2xl text-base font-semibold md:font-bold md:mb-0 md:min-h-full group-hover:text-gray-500">
-                              {course.name}
-                            </div>
-                            <p className="md:mb-5 mb-2 md:text-lg text-sm text-gray-700">
-                              {course.code}
-                            </p>
-                            <div className="mb-2 box-border grid grid-cols-1 md:grid-cols-2">
-                              <div className="pb-2 text-center text-xs md:text-left">
-                                <span className="mr-2 inline-block rounded-full border border-orange-400 px-2 py-0.5 text-orange-400 ">
-                                  {course.category}
-                                </span>
-                              </div>
-                              <div className="box-border text-center md:text-right ">
-                                <span className="rounded-sm bg-[#4d40f7] px-2 py-0.5 text-xs font-medium text-[#ffee00]">
-                                  {course.seat}
-                                </span>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </Link>
+                      <CardCourse key={course.id} course={course} />
                     </>
                   );
                 })
