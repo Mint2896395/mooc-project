55b8da9dd47cb728b76d19d0419af09e31784034
mint289365 <mint289365@gmail.com>
2023-07-05 06:41:03 +0700

    News rightbar mobile

---
+++ components/NewsCardSlider.tsx
@@ -0,0 +1,168 @@
+import React from "react";
+import { useEffect, useState } from 'react';
+import * as $ from "jquery";
+
+declare global {
+  interface Window {
+    $: any;
+    jQuery: any;
+  }
+}
+
+if (typeof window !== "undefined") {
+  window.$ = window.jQuery = $;
+}
+
+import dynamic from "next/dynamic";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+
+import CardCourse from "./Card";
+import getAllCourse from "~/pages/lib/helper";
+import Link from "next/link";
+
+// This is for Next.js. On Rect JS remove this line
+const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
+  ssr: false,
+});
+
+// This is for React JS, Remove this for Next.js
+// import OwlCarousel from 'react-owl-carousel';
+
+// Owlcarosuesl arrow Next
+const nextIcon =
+  '<button aria-label="slide forward" class="absolute flex items-center justify-center z-30 right-0 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400  cursor-pointer md:w-8 md:h-8 w-6 h-6 rounded-full bg-[var(--global-color-primary)] hover:bg-orange-400 text-white opacity-90" id="next"><svg class="dark:text-gray-900" width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L7 7L1 13" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /></svg></button>';
+
+// Owlcarosuesl arrow Pre 
+const prevIcon =
+  '<button aria-label="slide backward" class="absolute flex items-center justify-center z-30 left-0 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer md:w-8 md:h-8 w-6 h-6 rounded-full bg-[var(--global-color-primary)] hover:bg-orange-400 text-white opacity-90" id="prev"><svg class="dark:text-gray-900" width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M7 1L1 7L7 13" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /></svg></button>';
+
+interface NewsItem {
+  source: {
+    id: number;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+}
+
+const baseUrl = process.env.API_BASE_URL || "http://localhost:3000";
+
+const NewsCardSlider = () => {
+  const [newsItems, setNewsItems] = useState<NewsItem[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${baseUrl}/api/news`);
+        const data = await response.json();
+        setNewsItems(data.newses || []);
+      } catch (error) {
+        console.error('Error fetching news data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const options = {
+    margin: 5,
+    responsiveClass: true,
+    nav: true,
+    dots: false,
+    autoplay: false,
+    smartSpeed: 1000,
+    navClass: ["owl-prev", "owl-next"],
+    navText: [prevIcon, nextIcon],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        margin: 10,
+        items: 2,
+      },
+      600: {
+        items: 2,
+      },
+      700: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  };
+
+  return (
+    <div className="relative w-full mx-auto md:hidden ">
+      <div
+        className="box-border flex h-full transition duration-700 ease-out"
+        id="owl-carousel-products"
+      >
+        <ul id="owl-carousel-ul" className="owl-carousel owl-loaded owl-drag">
+          <OwlCarousel loop {...options}>
+            {newsItems && newsItems.length > 0
+              ? newsItems.slice(0, 5).map((news) => {
+                  return (
+                    <>
+                      <Link href={`/news/${news.source.id}`} key={news.source.id}>
+                        <div className="box-border block md:block lg:hidden ">
+                            <div className="relative box-border ">
+                              <div className="relative box-border block w-full z-[1]">
+                                <div className="relative box-border overflow-hidden ">
+                                  <div className="relative box-border touch-manipulation ">
+                                    <div className="relative box-border min-h-[1px]">
+                                      <div className="box-border ">
+                                        <div className="box-border flex flex-wrap ">
+                                          <div className="-mt-0 box-border w-full max-w-full flex-shrink-0 ">
+                                            <div className="mb-3 box-border overflow-hidden rounded-xl ">
+                                              <div className="relative box-border overflow-hidden rounded-xl pt-[78%]">
+                                                <img className="mx-auto absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4 w-[100%] align-middle " src={news.urlToImage} />
+                                                {/* <Image
+                                                  className="w-full rounded-xl"
+                                                  src={news.urlToImage}
+                                                  width={640}
+                                                  height={360}
+                                                  alt={news.source.name}
+                                                /> */}
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div className="box-border flex flex-wrap ">
+                                          <div className="-mt-0 box-border w-full max-w-full flex-shrink-0 ">
+                                            <div className="float-left mb-1 box-border text-xs font-medium tracking-normal text-[var(--global-color-primary)]">20 มิถุนายน 2566</div>
+                                          </div>
+                                        </div>
+                                        <div className="box-border flex flex-wrap ">
+                                          <div className="-mt-0 box-border w-full max-w-full flex-shrink-0 ">
+                                            <div className="mb-0 box-border ">
+                                              <p className="my-0 box-border overflow-hidden text-sm font-medium tracking-normal text-[rgba(37,37,37,1)]"> {news.title}</p>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="absolute bottom-0 left-1/2 mt-0 box-border hidden text-center "></div>
+                              </div>
+                            </div>
+                        </div>
+                      </Link>
+                    </>
+                  );
+                })
+              : ""}
+          </OwlCarousel>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default NewsCardSlider;
--- src/pages/news/[id].tsx
+++ src/pages/news/[id].tsx
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowLeft,
 } from "@fortawesome/free-solid-svg-icons";
+import NewsCardSlider from "components/NewsCardSlider";
 
 interface News {
   source: {
@@ -49,12 +50,12 @@ export default function News({ news }: NewsProps) {
       </center>
       <article>
         {selectedNews ? (
-          <div className="box-border pb-36 py-20">
+          <div className="box-border md:pb-36 pb-[65px] md:py-20 py-[50px]">
             <div className="box-border ">
-              <div className="box-border w-[88%] px-[15px] mx-auto">
+              <div className="box-border md:w-[88%] w-full px-[15px] mx-auto">
                 <div className="box-border ">
                   <div className="box-border flex flex-wrap justify-center ">
-                    <div className="box-border w-full max-w-full flex-shrink-0 pr-5 md:w-11/12 lg:w-3/4 ">
+                    <div className="box-border w-full max-w-full flex-shrink-0 md:pr-5 pr-0 md:w-11/12 lg:w-3/4 ">
                       <div className="box-border flex flex-wrap ">
                         <div className="box-border w-full max-w-full flex-shrink-0 ">
                           <div className="float-left mb-2 box-border text-sm font-medium tracking-normal text-[var(--global-color-primary)]">20 มิถุนายน 2566</div>
@@ -87,15 +88,15 @@ export default function News({ news }: NewsProps) {
                       </div>
                     </div>
                     <div className="-mt-0 box-border w-full max-w-full flex-shrink-0 md:w-full lg:w-1/4 ">
-                      <div className="mb-14 box-border bg-[#fef8f4] py-[25px] px-5">
+                      <div className="md:mb-14 mb-[30px] box-border bg-[#fef8f4] py-[25px] px-5">
                         <div className="box-border flex flex-wrap ">
-                          <div className="-mt-0 box-border w-full max-w-full flex-shrink-0 font-[Noto_Sans_Thai]">
-                            <h5 className="relative mt-0 mb-10 box-border pb-2 text-center text-2xl font-medium tracking-wide leading-[normal] text-[rgba(17,17,17,1)] border-b-[3px] border-[var(--global-color-primary)] ">ข่าวสารและบทความล่าสุด</h5>
+                          <div className="-mt-0 box-border w-full max-w-full flex-shrink-0 font-[Noto_Sans_Thai] md:px-0 px-2">
+                            <h5 className="relative mt-0 mb:mb-10 mb-[28px] mx-auto box-border md:pb-2 pb-0 text-center md:text-2xl text-lg font-medium tracking-wide leading-[normal] text-[rgba(17,17,17,1)] md:border-b-[3px] border-b-[2px] border-[var(--global-color-primary)] md:block table">ข่าวสารและบทความล่าสุด</h5>
                           </div>
                         </div>
                         {/* <!-- LASTEST NEWS :: PC & IPAD PRO --> */}
                         <div className="box-border hidden md:hidden lg:block ">
-                          <div className="-mt-0 box-border w-full max-w-full flex-shrink-0 md:w-1/3 lg:w-full ">
+                          <div className="-mt-0 box-border w-full max-w-full flex-shrink-0 md:w-1/3 lg:w-full mb-[30px] ">
                             {news?.slice(0, 5).map((news) => {
                                 return (
                                   <>                                    
@@ -138,72 +139,27 @@ export default function News({ news }: NewsProps) {
                           </div>
                         </div>
                         {/* <!-- LASTEST NEWS :: IPAD & MOBILE --> */}
-                        <div className="box-border block md:block lg:hidden ">
-                          <div className="relative box-border ">
-                            <div className="relative mb-20 box-border block w-full z-[1]">
-                              <div className="relative box-border overflow-hidden ">
-                                <div className="relative box-border touch-manipulation ">
-                                  <div className="relative float-left box-border min-h-[1px]">
-                                    <div className="box-border ">
-                                      <a className="mb-9 box-border hover:block text-[rgba(13,110,253,1)]" href="https://mooc.chula.ac.th/news-detail/11">
-                                        <div className="box-border flex flex-wrap ">
-                                          <div className="-mt-0 box-border w-full max-w-full flex-shrink-0 ">
-                                            <div className="mb-3 box-border overflow-hidden rounded-xl ">
-                                              <div className="relative box-border overflow-hidden rounded-xl pt-[78%]">
-                                                <img className="absolute top-1/2 left-1/2 box-border block h-full w-full align-middle " src="https://mooc.chula.ac.th/img/upload/ปกหน้า_ChatGPT ถอยไป.png" />
-                                              </div>
-                                            </div>
-                                          </div>
-                                        </div>
-                                        <div className="box-border flex flex-wrap ">
-                                          <div className="-mt-0 box-border w-full max-w-full flex-shrink-0 ">
-                                            <div className="float-left mb-1 box-border text-xs font-medium tracking-normal text-[rgba(219,95,142,1)]">20 มิถุนายน 2566</div>
-                                          </div>
-                                        </div>
-                                        <div className="box-border flex flex-wrap ">
-                                          <div className="-mt-0 box-border w-full max-w-full flex-shrink-0 ">
-                                            <div className="mb-0 box-border ">
-                                              <p className="my-0 box-border overflow-hidden text-base font-medium tracking-normal text-[rgba(37,37,37,1)]">ขอเชิญชาวจุฬาฯ และบุคคลทั่วไป เข้าฟังบรรยายในหัวข้อ: ChatGPT ถอยไป!! มาปล่อยพลัง AI ด้วย Microsoft Bing x GPT4</p>
-                                            </div>
-                                          </div>
-                                        </div>
-                                      </a>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                              <div className="mt-2.5 box-border text-center z-[99]">
-                                <button className="absolute top-1/2 box-border inline-block h-10 w-10 cursor-default rounded-sm text-sm normal-case text-white opacity-50 open:cursor-pointer font-[inherit] leading-[inherit] shadow-[none] z-[99]" role="presentation" type="button">
-                                  <i className="absolute top-1/2 left-1/2 box-border text-sm text-white "></i>
-                                </button>
-                                <button className="absolute top-1/2 box-border inline-block h-10 w-10 cursor-pointer rounded-sm text-sm normal-case text-white open:cursor-pointer font-[inherit] leading-[inherit] shadow-[none] z-[99]" role="presentation" type="button">
-                                  <i className="absolute top-1/2 left-1/2 box-border text-sm text-white "></i>
-                                </button>
-                              </div>
-                              <div className="absolute bottom-0 left-1/2 mt-0 box-border hidden text-center "></div>
-                            </div>
-                          </div>
-                        </div>
+                        <NewsCardSlider />
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className="relative box-border flex w-full max-w-full flex-col items-center">
-                  <div className="box-border ">
-                    <Link
-                      className="group box-border min-w-[140px] rounded-3xl border border-[var(--global-color-primary)] px-6 py-2 text-center font-[inherit] text-sm font-medium normal-case leading-[inherit] tracking-normal text-[var(--global-color-primary)] cursor-pointer hover:bg-[var(--global-color-primary)]"
-                      href="/news"
-                    >
-                      <FontAwesomeIcon
-                        icon={faArrowLeft}
-                        style={{ width: "10px" }}
-                        className="box-border inline-block not-italic leading-none text-[var(--global-color-primary)] group-hover:text-white"
-                      />
-                      <span className="group-hover:text-white ">
-                        &nbsp;&nbsp;ย้อนกลับ
-                      </span>
-                    </Link>
-                  </div>
+              </div>
+              <div className="relative box-border flex w-full max-w-full flex-col items-center mt-5">
+                <div className="box-border ">
+                  <Link
+                    className="group box-border rounded-3xl border border-[var(--global-color-primary)] px-6 py-2 text-center font-[inherit] text-sm font-medium normal-case leading-[inherit] tracking-normal text-[var(--global-color-primary)] cursor-pointer hover:bg-[var(--global-color-primary)]"
+                    href="/news"
+                  >
+                    <FontAwesomeIcon
+                      icon={faArrowLeft}
+                      style={{ width: "10px" }}
+                      className="box-border inline-block not-italic leading-none text-[var(--global-color-primary)] group-hover:text-white"
+                    />
+                    <span className="group-hover:text-white ">
+                      &nbsp;&nbsp;ย้อนกลับ
+                    </span>
+                  </Link>
                 </div>
               </div>
             </div>
