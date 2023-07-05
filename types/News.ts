a5bc75d068de13115d3818f9d571a53943a496cc
mint289365 <mint289365@gmail.com>
2023-07-05 20:05:25 +0700

    using swr 2

--- types/News.ts
+++ types/News.ts
@@ -1,16 +1,15 @@
 interface News {
-    [x: string]: number;
-    source: {
-        id: null | number;
-        name: string;
-    };
-    author: string;
-    title: string;
-    description: string;
-    url: string;
-    urlToImage: string;
-    publishedAt: string;
-    content: string;
+  source: {
+    id: null | number;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
 }
 
 export default News;
