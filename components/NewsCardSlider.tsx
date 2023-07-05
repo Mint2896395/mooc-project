f762568ba8ff1db99a72e6283ff6bc0a7c383767
mint289365 <mint289365@gmail.com>
2023-07-05 17:01:59 +0700

    vercel_url 2

--- components/NewsCardSlider.tsx
+++ components/NewsCardSlider.tsx
@@ -57,7 +57,7 @@ interface NewsItemProp {
   newses: NewsItem[];
 }
 
-const baseUrl = process.env.VERCEL_URL || "http://localhost:3000";
+const baseUrl = process.env.API_BASE_URL || "http://localhost:3000";
 
 const NewsCardSlider = () => {
   const [newsItems, setNewsItems] = useState<NewsItem[]>([]);
@@ -96,7 +96,7 @@ const NewsCardSlider = () => {
 
     const fetchData = async () => {
       try {
-        const response = await fetch(`${baseUrl}/api/news`);
+        const response = await fetch(`https://vercel.com/api/news`);
         const data = await response.json();
         setNewsItems(data.newses || []);
       } catch (error) {
